0359745810dafbfeb860395089cf851298e1f3ce
usamaeltmsah <usamaeltmsah@gmail.com>
2020-09-22 02:34:58 +0200

    Chage between the players

--- Game.rb
+++ Game.rb
@@ -6,7 +6,7 @@ DICTIONARY_FILE = "dictionary.txt"
 class Game
     attr_reader :previous_player, :current_player
     def initialize(players)
-        @previous_player, @current_player = players
+        @current_player, @previous_player = players
         @fragment = ""
         @dictionary = read_dictionary
     end
@@ -15,5 +15,9 @@ class Game
         dictionary = Set[]
         File.foreach(DICTIONARY_FILE) { |word| dictionary.add(word.chomp) }
         dictionary
+    end
+    
+    def next_player!
+        @previous_player, @current_player = @current_player, @previous_player
     end
 end
